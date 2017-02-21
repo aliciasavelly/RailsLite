4365692ce1cf726cb9171e93fffeda8c146d42d2
aliciasavelly <alicia.savelly@gmail.com>
2017-02-21 14:22:42 -0800

    raise error if double render

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -14,17 +14,22 @@ class ControllerBase
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise if already_built_response?
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    
+    raise if already_built_response?
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
